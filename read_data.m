abd24bc2bf5d984d6f46f261f7f4305b5c50b5d8
Xiao He <xiao_he@me.com>
2017-09-05 10:44:15 +0200

    Update read_data.m

--- read_data.m
+++ read_data.m
@@ -1,12 +1,12 @@
 function [X, y, z, k] = read_data(data)
 
 if strcmp(data, 'Face')
-    load('../data/Face.mat');
+    load('Face.mat');
     y = pose + 1;
     z = identity + 1;
     k = length(unique(y));  
 elseif strcmp(data, 'Face4')
-    load('../data/Face.mat');
+    load('Face.mat');
     y = pose + 1;
     z = identity + 1;
     X = X(identity<4, :);
@@ -14,35 +14,35 @@ elseif strcmp(data, 'Face4')
     y = y(identity<4);
     k = length(unique(y));          
 elseif strcmp(data, 'Multi')
-    load('../data/Multi.mat');
+    load('Multi.mat');
     z = Z';
     y = y';
     k = length(unique(y));          
 elseif strcmp(data, 'DLBCL')
-    load('../data/DLBCL.mat');
+    load('DLBCL.mat');
     z = Z';
     y = y';
     k = length(unique(y));        
 elseif strcmp(data, 'Webkb')
-    load('../data/Webkb.mat');
+    load('Webkb.mat');
     X = double(full(X));
     z = double(y2' + 1);
     y = double(y1' + 1);
     k = length(unique(y));     
 elseif strcmp(data, 'Simu1')
-    load('../data/Simu1.mat');
+    load('Simu1.mat');
     X = double(X);
     z = double(z' + 1);
     y = double(y' + 1);
     k = length(unique(y));    
 elseif strcmp(data, 'Simu2')
-    load('../data/Simu2.mat');
+    load('Simu2.mat');
     X = double(X);
     z = double(z' + 1);
     y = double(y' + 1);
     k = length(unique(y));  
 elseif strcmp(data, 'Simu3')
-    load('../data/Simu3.mat');
+    load('Simu3.mat');
     X = double(X);
     z = double(z');
     y = double(y' + 1);
